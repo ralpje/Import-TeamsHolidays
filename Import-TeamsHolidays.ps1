23494595af5ea9556af43fdb709f0d1781a5e9a2
Ralph Eckhard <ralph@ralpheckhard.com>
2024-03-26 09:48:51 +0100

    Parameter toegevoegd

--- Import-TeamsHolidays.ps1
+++ Import-TeamsHolidays.ps1
@@ -1,6 +1,9 @@
-$holidays = invoke-webrequest https://date.nager.at/api/v3/publicholidays/2024/NL | convertfrom-json
+[param]
+
+
+$holidays = Invoke-WebRequest https://date.nager.at/api/v3/publicholidays/2024/NL | ConvertFrom-Json
 foreach ($holiday in $holidays) {
-$daterange = New-CSOnlineDateTimeRange -Start (get-date $holiday.date -Format "d/M/yyyy")
-$holidayname = $holiday.localname + "-" + (get-date $holiday.date -Format "yyyy")
-New-CSOnlineSchedule -Name $holidayname -FixedSchedule -DateTimeRanges @($daterange)
+    $daterange = New-CsOnlineDateTimeRange -Start (Get-Date $holiday.date -Format 'd/M/yyyy')
+    $holidayname = $holiday.localname + '-' + (Get-Date $holiday.date -Format 'yyyy')
+    New-CsOnlineSchedule -Name $holidayname -FixedSchedule -DateTimeRanges @($daterange)
 }
